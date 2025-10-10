06c7b6018cfb44ce859ff5f9f164f6c17ca01ebc
aveenbrifcany <aveenbrifcany@gmail.com>
2025-10-09 21:28:07 -0500

    WIP

--- Prescribers/scripts.sql
+++ Prescribers/scripts.sql
@@ -93,10 +93,38 @@ select c.generic_name as drug_name,
 
 --     b. Which drug (generic_name) has the hightest total cost per day? **Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.**
 
+select *
+from prescription
+
+select d.generic_name,
+       round(SUM(p.total_drug_cost) / SUM(p.total_day_supply) ,2) AS avg_daily_cost
+from drug AS d
+inner join prescription AS p
+    on p.drug_name = d.drug_name
+group by d.generic_name
+order by avg_daily_cost DESC
+limit 5;
+
 -- 4. 
 --     a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs. **Hint:** You may want to use a CASE expression for this. See https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-case/ 
 
+select drug_name, 
+  case when opioid_drug_flag = 'Y' then 'opioid' 
+       when antibiotic_drug_flag = 'Y' then 'antibiotic'
+	   else 'neither' end as drug_type
+from drug;
+	  
 --     b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
+
+select drug_name, total_drug_cost as mony,
+  case when opioid_drug_flag = 'Y' then 'opioid' 
+       when antibiotic_drug_flag = 'Y' then 'antibiotic'
+	   else 'neither' end as drug_type
+from drug as d
+inner join prescription as p
+using(drug_name)
+group by drug_name, total_drug_cost, drug_type
+order by mony DESC;
 
 -- 5. 
 --     a. How many CBSAs are in Tennessee? **Warning:** The cbsa table contains information for all states, not just Tennessee.
